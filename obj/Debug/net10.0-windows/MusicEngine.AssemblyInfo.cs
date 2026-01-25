219fcf47f073888165fe949e7da33ade677dc1c1
watermann420 <watermannog@gmail.com>
2026-01-25 21:38:24 +0100

    Ui Update

--- obj/Debug/net10.0-windows/MusicEngine.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/MusicEngine.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicEngine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc5f222dd6a01bd20d3fb6ccace732bffa2b6030")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+466fedeece0da211bc45512a1d30f7a54239de1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicEngine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicEngine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
