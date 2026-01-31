7cc69bc31fcd8250aee36e9eb15faf16f7ae1ca0
watermann420 <watermannog@gmail.com>
2026-01-31 07:54:12 +0100

    Performence

--- obj/Debug/net10.0-windows/MusicEngine.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/MusicEngine.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicEngine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bed7d189c54e4720e15c6571d6ea7b5a6280fe1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+938f0644db46d5a8dc857cce415bd841a675da99")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicEngine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicEngine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
